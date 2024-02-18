aec64b7b6e02d4a2766d6532d8b9569b826f11f3
JH-TT <leejongho9803@gmail.com>
2024-02-19 00:22:49 +0900

    feat: 예약정보 공연 아이디로만 연결되도록 변경

--- src/main/java/com/wanted/preonboarding/model/reservation/Reservation.java
+++ src/main/java/com/wanted/preonboarding/model/reservation/Reservation.java
@@ -1,24 +1,19 @@
 package com.wanted.preonboarding.model.reservation;
 
+import com.wanted.preonboarding.model.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table
@@ -26,14 +21,13 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-@EntityListeners(AuditingEntityListener.class)
-public class Reservation {
+@SQLRestriction("deleted_at IS NULL")
+public class Reservation extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "performance_id", columnDefinition = "BINARY(16)", nullable = false)
-    private Performance performance;
+    @Column(columnDefinition = "BINARY(16)", nullable = false, name = "performance_id")
+    private UUID performanceId;
     @Column(nullable = false)
     private String name;
     @Column(nullable = false)
@@ -46,8 +40,4 @@ public class Reservation {
     private String line;
     @Column(nullable = false)
     private int seat;
-    @CreatedDate
-    private LocalDateTime createdAt;
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 }
