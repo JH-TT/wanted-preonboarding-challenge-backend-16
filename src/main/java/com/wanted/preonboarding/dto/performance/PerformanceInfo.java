2c202a614e32a5adca21545ab1412ea5aaca55b9
JH-TT <leejongho9803@gmail.com>
2024-02-29 01:36:06 +0900

    add: 공연ID 추가
    - 공연ID를 넣어줌으로써 데이터 조회등 특정 공연에 대한 메서드에 쉽게 사용한다.

--- src/main/java/com/wanted/preonboarding/dto/performance/PerformanceInfo.java
+++ src/main/java/com/wanted/preonboarding/dto/performance/PerformanceInfo.java
@@ -1,6 +1,7 @@
 package com.wanted.preonboarding.dto.performance;
 
 import com.wanted.preonboarding.model.performance.Performance;
+import java.util.UUID;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Builder
 public class PerformanceInfo {
     private String name; // 공연명
+    private UUID performanceId; // 공연/전시회 ID
     private int round; // 회차
     private int type; // 공연인지 전시회 인지
     private String startDate; // 시작 일시
@@ -18,6 +20,7 @@ public class PerformanceInfo {
     public static PerformanceInfo of(Performance perform) {
         return PerformanceInfo.builder()
                 .name(perform.getName())
+                .performanceId(perform.getId())
                 .round(perform.getRound())
                 .type(perform.getType())
                 .startDate(perform.getStartDate().toString())
