93397f764c5b84ea4c83e5439287d1de4dfc6f58
JH-TT <leejongho9803@gmail.com>
2024-03-10 03:05:28 +0900

    feat: 예약한 유저정보 객체 생성

---
+++ src/main/java/com/wanted/preonboarding/dto/reservation/ReservationUserInfo.java
@@ -0,0 +1,11 @@
+package com.wanted.preonboarding.dto.reservation;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReservationUserInfo {
+    private String userName;
+    private String phoneNumber;
+}
