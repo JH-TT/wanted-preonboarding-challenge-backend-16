470e76683cd4e439f440d3e1bfe84f9c861f6d0b
JH-TT <leejongho9803@gmail.com>
2024-05-12 16:25:16 +0900

    test: 테스트 내용 수정
    - 타입변경, 메서드 이름명 변경으로 인한 테스트창 수정
    - 영속성 관련 테스트 추가

--- src/test/java/com/wanted/preonboarding/model/reservation/ReservationTest.java
+++ src/test/java/com/wanted/preonboarding/model/reservation/ReservationTest.java
@@ -1,9 +1,9 @@
 package com.wanted.preonboarding.model.reservation;
 
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.wanted.preonboarding.dto.reservation.ReservationInfo;
 import com.wanted.preonboarding.dto.reservation.ReservationRequest;
 import com.wanted.preonboarding.dto.reservation.ReservationResponse;
 import com.wanted.preonboarding.dto.reservation.ReservationUserInfo;
@@ -14,11 +14,16 @@ import com.wanted.preonboarding.repository.PerformanceRepository;
 import com.wanted.preonboarding.repository.PerformanceSeatInfoRepository;
 import com.wanted.preonboarding.repository.ReservationRepository;
 import com.wanted.preonboarding.service.reservation.ReservationService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.assertj.core.api.Assertions;
+import org.hibernate.TransientPropertyValueException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +43,9 @@ class ReservationTest {
     @Autowired
     private ReservationService reservationService;
 
+    @PersistenceContext
+    private EntityManager em;
+
     private static final String PERFORMANCE_NAME = "게르테나 미술관";
     private static final String USER_NAME = "이종호";
     private static final String PHONE_NUMBER = "010-1234-5678";
@@ -52,7 +60,7 @@ class ReservationTest {
         for (int i = 1; i < 5; i++) {
             PerformanceSeatInfo seatInfo = getSampleSeat(savedPerformance, i);
             performanceSeatInfoRepository.save(seatInfo);
-            performance.reserveSeat(seatInfo);
+            performance.registerSeat(seatInfo);
         }
 
         // when
@@ -69,8 +77,8 @@ class ReservationTest {
         PerformanceSeatInfo seatInfo = performanceSeatInfoRepository.findPerformanceSeatInfo(savedPerformance, 1, "A", 1)
                 .orElseThrow(() -> new IllegalArgumentException("좌석이 없습니다."));
 
-        Assertions.assertThat(reservationList.size()).isEqualTo(1);
-        Assertions.assertThat(seatInfo.getIsReserve()).isEqualTo("disable");
+        assertThat(reservationList.size()).isEqualTo(1);
+        assertThat(seatInfo.getIsReserve()).isEqualTo("disable");
     }
 
     @Test
@@ -86,7 +94,7 @@ class ReservationTest {
         // then
         Throwable exception = assertThrows(IllegalArgumentException.class,
                 () -> reservationService.reserve(request));
-        Assertions.assertThat(exception.getMessage()).isEqualTo("공연이 없습니다.");
+        assertThat(exception.getMessage()).isEqualTo("공연이 없습니다.");
     }
 
     @Test
@@ -103,7 +111,7 @@ class ReservationTest {
         // then
         Throwable exception = assertThrows(IllegalArgumentException.class,
                 () -> reservationService.reserve(request));
-        Assertions.assertThat(exception.getMessage()).isEqualTo("해당 좌석이 없습니다.");
+        assertThat(exception.getMessage()).isEqualTo("해당 좌석이 없습니다.");
     }
 
     @Test
@@ -116,18 +124,18 @@ class ReservationTest {
         for (int i = 1; i < 5; i++) {
             PerformanceSeatInfo seatInfo = getSampleSeat(savedPerformance, i);
             performanceSeatInfoRepository.save(seatInfo);
-            performance.reserveSeat(seatInfo);
+            performance.registerSeat(seatInfo);
         }
 
         // when
         ReservationRequest request = getReservationRequest(savedPerformance.getId(), savedPerformance.getName(), 100000, false);
-        reservationService.reserve(request); // 첫 예약
+        reservationService.reserve(request);// 첫 예약
 
         // then
         Throwable exception = assertThrows(
                 IllegalArgumentException.class,
                 () -> reservationService.reserve(request));// 이미 예약된 자리니까 예외처리가 나와야 한다.
-        Assertions.assertThat(exception.getMessage()).isEqualTo("예약할 수 없는 좌석입니다.");
+        assertThat(exception.getMessage()).isEqualTo("예약할 수 없는 좌석입니다.");
     }
 
     @Test
@@ -140,7 +148,7 @@ class ReservationTest {
         for (int i = 1; i < 5; i++) {
             PerformanceSeatInfo seatInfo = getSampleSeat(savedPerformance, i);
             performanceSeatInfoRepository.save(seatInfo);
-            performance.reserveSeat(seatInfo);
+            performance.registerSeat(seatInfo);
         }
 
         // when
@@ -148,7 +156,7 @@ class ReservationTest {
 
         // then
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> reservationService.reserve(request));
-        Assertions.assertThat(exception.getMessage()).isEqualTo("잔액이 부족합니다.");
+        assertThat(exception.getMessage()).isEqualTo("잔액이 부족합니다.");
     }
 
     @Test
@@ -161,7 +169,7 @@ class ReservationTest {
         for (int i = 1; i < 5; i++) {
             PerformanceSeatInfo seatInfo = getSampleSeat(savedPerformance, i);
             performanceSeatInfoRepository.save(seatInfo);
-            performance.reserveSeat(seatInfo);
+            performance.registerSeat(seatInfo);
         }
 
         ReservationRequest request = getReservationRequest(savedPerformance.getId(), savedPerformance.getName(), 100000, false);
@@ -172,10 +180,10 @@ class ReservationTest {
         usrInfo.setPhoneNumber(PHONE_NUMBER);
         usrInfo.setUserName(USER_NAME);
 
-        List<ReservationInfo> reservationResponses = reservationService.reservationList(usrInfo);
+        List<ReservationResponse> reservationResponses = reservationService.reservationList(usrInfo);
 
         // then
-        Assertions.assertThat(reservationResponses.size()).isEqualTo(1);
+        assertThat(reservationResponses.size()).isEqualTo(1);
     }
 
     @Test
@@ -188,7 +196,7 @@ class ReservationTest {
         for (int i = 1; i < 5; i++) {
             PerformanceSeatInfo seatInfo = getSampleSeat(savedPerformance, i);
             performanceSeatInfoRepository.save(seatInfo);
-            performance.reserveSeat(seatInfo);
+            performance.registerSeat(seatInfo);
         }
 
         // when
@@ -197,7 +205,7 @@ class ReservationTest {
         System.out.println(request.getSalesList());
         // then
         // 첫 결제라 10% 할인 -> 18000원만 결제
-        Assertions.assertThat(request.getBalance()).isEqualTo(82000);
+        assertThat(request.getBalance()).isEqualTo(82000);
     }
 
     @Test
@@ -210,7 +218,7 @@ class ReservationTest {
         for (int i = 1; i < 5; i++) {
             PerformanceSeatInfo seatInfo = getSampleSeat(savedPerformance, i);
             performanceSeatInfoRepository.save(seatInfo);
-            performance.reserveSeat(seatInfo);
+            performance.registerSeat(seatInfo);
         }
 
         // when
@@ -220,7 +228,7 @@ class ReservationTest {
 
         // then
         // 첫 결제 10% + 일주일전 결제 + 2000원 -> 총 4000원 할인으로 16000원만 계산
-        Assertions.assertThat(request.getBalance()).isEqualTo(84000);
+        assertThat(request.getBalance()).isEqualTo(84000);
     }
 
     @DisplayName("여러자리 예약 성공테스트")
@@ -231,29 +239,36 @@ class ReservationTest {
         Performance savedPerformance = performanceRepository.save(performance);
 
         for (int i = 1; i < 5; i++) {
-            PerformanceSeatInfo seatInfo = getSampleSeat(savedPerformance, i);
+            PerformanceSeatInfo seatInfo = getSampleSeat(performance, i);
+            performance.registerSeat(seatInfo);
             performanceSeatInfoRepository.save(seatInfo);
-            performance.reserveSeat(seatInfo);
         }
+        em.flush(); // performance가 IDENTITY 전략이 아니라서 db에 저장되지 않았었음.
 
         // when
         ReservationRequest reservationRequest = getReservationRequest(savedPerformance.getId(),
                 savedPerformance.getName(), 100000, true);
         ReservationResponse reserve = reservationService.reserve(reservationRequest);
-        System.out.println("========== 2자리 예약 결과 ==========");
+        System.out.println("========== 3자리 예약 결과 ==========");
         System.out.println(reserve);
 
         // then
-        List<Reservation> reservationList = reservationRepository.findAllByUserNameAndPhoneNumber(USER_NAME,
-                PHONE_NUMBER);
+        Reservation reservation = reservationRepository.findById(reserve.getReservationId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 예약이 없습니다."));
+        //좌석이 3자리여야 한다.
+        List<String> seats = reservation.getReservedSeats();
+
         PerformanceSeatInfo seatInfo1 = performanceSeatInfoRepository.findPerformanceSeatInfo(savedPerformance, 1, "A", 1)
                 .orElseThrow(() -> new IllegalArgumentException("좌석이 없습니다."));
-        PerformanceSeatInfo seatInfo2 = performanceSeatInfoRepository.findPerformanceSeatInfo(savedPerformance, 1, "A", 1)
+        PerformanceSeatInfo seatInfo2 = performanceSeatInfoRepository.findPerformanceSeatInfo(savedPerformance, 1, "A", 2)
+                .orElseThrow(() -> new IllegalArgumentException("좌석이 없습니다."));
+        PerformanceSeatInfo seatInfo3 = performanceSeatInfoRepository.findPerformanceSeatInfo(savedPerformance, 1, "A", 3)
                 .orElseThrow(() -> new IllegalArgumentException("좌석이 없습니다."));
 
-        Assertions.assertThat(reservationList.size()).isEqualTo(2);
-        Assertions.assertThat(seatInfo1.getIsReserve()).isEqualTo("disable");
-        Assertions.assertThat(seatInfo2.getIsReserve()).isEqualTo("disable");
+        assertThat(Arrays.stream(reserve.getSeatInfos().split(", ")).toList().size()).isEqualTo(seats.size());
+        assertThat(seatInfo1.getIsReserve()).isEqualTo("disable");
+        assertThat(seatInfo2.getIsReserve()).isEqualTo("disable");
+        assertThat(seatInfo3.getIsReserve()).isEqualTo("disable");
     }
 
     @DisplayName("삭제 테스트")
@@ -266,7 +281,7 @@ class ReservationTest {
         for (int i = 1; i < 5; i++) {
             PerformanceSeatInfo seatInfo = getSampleSeat(savedPerformance, i);
             performanceSeatInfoRepository.save(seatInfo);
-            performance.reserveSeat(seatInfo);
+            performance.registerSeat(seatInfo);
         }
 
         ReservationRequest reservationRequest = getReservationRequest(savedPerformance.getId(),
@@ -277,6 +292,41 @@ class ReservationTest {
 //        reservationService.deleteReservation();
 
         // then
+    }
+
+    @DisplayName("좌석 영속성 테스트")
+    @Test
+    public void 좌석_영속성_테스트() throws Exception {
+        // given
+        Performance performance = getSamplePerformance(PERFORMANCE_NAME, 20000, 1, 0, LocalDateTime.now().plusDays(1), "enable"); // 게르테나 미술관 전시회 정보
+        Performance savedPerformance = performanceRepository.save(performance);
+
+        for (int i = 1; i < 5; i++) {
+            PerformanceSeatInfo seatInfo = getSampleSeat(savedPerformance, i);
+            performanceSeatInfoRepository.save(seatInfo);
+            performance.registerSeat(seatInfo);
+        }
+
+        ReservationRequest reservationRequest = getReservationRequest(savedPerformance.getId(),
+                savedPerformance.getName(), 100000, false);
+
+        Reservation reservation = Reservation.toEntity(reservationRequest);
+
+        // when
+        PerformanceSeat performanceSeat = reservationRequest.getSeatList().get(0);
+        PerformanceSeatInfo performanceSeatInfo = performanceSeatInfoRepository.findPerformanceSeatInfo(
+                savedPerformance, performanceSeat.getGate(),
+                performanceSeat.getLine(), performanceSeat.getSeat()).orElseThrow(
+                IllegalArgumentException::new
+        );
+
+        reservation.reserveSeat(performanceSeatInfo);
+
+        // then
+        Throwable exception = assertThrows(RuntimeException.class, () -> {
+            em.flush();
+        });
+        assertThat(exception.getMessage()).contains("org.hibernate.TransientPropertyValueException");
     }
 
     /**
@@ -327,6 +377,9 @@ class ReservationTest {
         if (more) {
             PerformanceSeat performanceSeat2 = new PerformanceSeat(1, "A", 2, "enable");
             seatList.add(performanceSeat2);
+
+            PerformanceSeat performanceSeat3 = new PerformanceSeat(1, "A", 3, "enable");
+            seatList.add(performanceSeat3);
         }
 
         ReservationRequest request = new ReservationRequest();
