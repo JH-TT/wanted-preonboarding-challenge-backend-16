00620d987f122a205b282681eb6690fd3fcc0946
JH-TT <leejongho9803@gmail.com>
2024-05-19 03:40:44 +0900

    fix: salesList 타입 변경

--- src/main/java/com/wanted/preonboarding/service/discount/DiscountUtils.java
+++ src/main/java/com/wanted/preonboarding/service/discount/DiscountUtils.java
@@ -7,10 +7,11 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DiscountUtils {
 
-    public static long pay(long cnt, long price, LocalDateTime startDate, LocalDateTime now, ArrayList<String> salesList) {
+    public static long pay(long cnt, long price, LocalDateTime startDate, LocalDateTime now, List<String> salesList) {
         long totalDiscount = 0;
         totalDiscount += firstPay(cnt, price, salesList);
         totalDiscount += payBeforeOneWeek(price, startDate, now, salesList);
@@ -19,21 +20,21 @@ public class DiscountUtils {
         return price - totalDiscount; // 최종 결제할 금액
     }
 
-    private static long firstPay(long cnt, long price, ArrayList<String> salesList) {
+    private static long firstPay(long cnt, long price, List<String> salesList) {
         if (cnt > 1) return 0;
         DiscountService discountService = new DiscountService(BigDecimal.valueOf(price));
         salesList.add(FIRST_PURCHASE.getDiscountDesc());
         return discountService.applyDiscount(FIRST_PURCHASE).longValue();
     }
 
-    private static long payBeforeOneWeek(long price, LocalDateTime startDate, LocalDateTime now, ArrayList<String> salesList) {
+    private static long payBeforeOneWeek(long price, LocalDateTime startDate, LocalDateTime now, List<String> salesList) {
         if (ChronoUnit.WEEKS.between(now, startDate) < 1) return 0;
         DiscountService discountService = new DiscountService(BigDecimal.valueOf(price));
         salesList.add(PURCHASE_BEFORE_ONE_WEEK.getDiscountDesc());
         return discountService.applyDiscount(PURCHASE_BEFORE_ONE_WEEK).longValue();
     }
 
-    private static long payPerTenTimes(long cnt, long price, ArrayList<String> salesList) {
+    private static long payPerTenTimes(long cnt, long price, List<String> salesList) {
         if (cnt == 0 || cnt % 10 != 0) return 0;
         DiscountService discountService = new DiscountService(BigDecimal.valueOf(price));
         salesList.add(PER_TEN_TIMES.getDiscountDesc());
