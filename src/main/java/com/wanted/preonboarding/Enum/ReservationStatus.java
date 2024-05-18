be5eadc11051c5fdfadbb72791a8c30e084ea204
JH-TT <leejongho9803@gmail.com>
2024-05-19 03:46:51 +0900

    feat: 예약 상태 Enum 수정
    - typeDesc필드 추가
    - 타입을 가져오는 기능 추가

--- src/main/java/com/wanted/preonboarding/Enum/ReservationStatus.java
+++ src/main/java/com/wanted/preonboarding/Enum/ReservationStatus.java
@@ -1,7 +1,25 @@
 package com.wanted.preonboarding.Enum;
 
+import java.util.Arrays;
+import org.apache.commons.lang3.StringUtils;
+
 public enum ReservationStatus {
-    ACTIVE,
-    CANCEL,
-    PERFORMANCE_CANCEL
+    ACTIVE("active"),
+    INACTIVE("inactive"),
+    CANCEL("cancel"),
+    PERFORMANCE_CANCEL("performance_cancel"),
+    EMPTY("Nothing");
+
+    private final String typeDesc;
+
+    ReservationStatus(String typeDesc) {
+        this.typeDesc = typeDesc;
+    }
+
+    public static ReservationStatus getType(String typeDesc) {
+        return Arrays.stream(ReservationStatus.values())
+                .filter(status -> StringUtils.equals(typeDesc, status.typeDesc))
+                .findAny()
+                .orElse(EMPTY);
+    }
 }
