933b0d138c89f7a7d2c0ee4b6c9a465f04afd2f6
JH-TT <leejongho9803@gmail.com>
2024-05-22 21:38:38 +0900

    test: 공연 취소 테스트 추가

--- src/test/java/com/wanted/preonboarding/model/PerformanceTest.java
+++ src/test/java/com/wanted/preonboarding/model/PerformanceTest.java
@@ -1,50 +1,190 @@
 package com.wanted.preonboarding.model;
 
+import com.wanted.preonboarding.dto.reservation.ReservationRequest;
+import com.wanted.preonboarding.dto.seat.PerformanceSeat;
 import com.wanted.preonboarding.model.performance.Performance;
 import com.wanted.preonboarding.model.performance.PerformanceSeatInfo;
+import com.wanted.preonboarding.model.reservation.Reservation;
 import com.wanted.preonboarding.repository.PerformanceRepository;
 import com.wanted.preonboarding.repository.PerformanceSeatInfoRepository;
+import com.wanted.preonboarding.repository.ReservationRepository;
+import com.wanted.preonboarding.service.performance.PerformanceService;
+import com.wanted.preonboarding.service.reservation.ReservationService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class PerformanceTest {
 
-    private final PerformanceRepository performanceRepository;
-    private final PerformanceSeatInfoRepository performanceSeatInfoRepository;
-
     @Autowired
-    PerformanceTest(PerformanceRepository performanceRepository,
-                    PerformanceSeatInfoRepository performanceSeatInfoRepository) {
-        this.performanceRepository = performanceRepository;
-        this.performanceSeatInfoRepository = performanceSeatInfoRepository;
+    private PerformanceRepository performanceRepository;
+    @Autowired
+    private ReservationRepository reservationRepository;
+    @Autowired
+    private ReservationService reservationService;
+    @Autowired
+    private PerformanceService performanceService;
+    @Autowired
+    private PerformanceSeatInfoRepository performanceSeatInfoRepository;
+    @PersistenceContext
+    private EntityManager em;
+    private static final String PERFORMANCE_NAME = "게르테나 미술관";
+    private static final String USER_NAME = "이종호";
+    private static final String PHONE_NUMBER = "010-1234-5678";
+    @Test
+    @DisplayName("공연 성공 테스트")
+    public void 공연_테스트() throws Exception {
+        // given
+        Performance performance = getSamplePerformance(PERFORMANCE_NAME, 20000, 1, 0, LocalDateTime.now(), "enable"); // 게르테나 미술관 전시회 정보
+        Performance savedPerformance = performanceRepository.save(performance);
+
+        Optional<Performance> byId = performanceRepository.findById(savedPerformance.getId());
+        System.out.println(byId.get());
     }
 
     @Test
-    @Transactional
-    public void 공연좌석_테스트() throws Exception {
+    @DisplayName("공연 취소로 인한 예약환불")
+    public void 공연_취소로_인한_예약환불() throws Exception {
         // given
-        Performance performance = Performance.builder()
-                .isReserve("a")
-                .name("하이")
-                .price(1000)
-                .round(1)
-                .type(1)
-                .startDate(LocalDateTime.now())
+        Performance performance = getSamplePerformance(PERFORMANCE_NAME, 20000, 1, 0, LocalDateTime.now().plusDays(1), "enable"); // 게르테나 미술관 전시회 정보
+        Performance savedPerformance = performanceRepository.save(performance);
+
+        for (int i = 1; i < 5; i++) {
+            PerformanceSeatInfo seatInfo = getSampleSeat(savedPerformance, i);
+            performanceSeatInfoRepository.save(seatInfo);
+            performance.registerSeat(seatInfo);
+        }
+
+        // 2개의 예약이 있다고 가정
+        for (int i = 1; i < 3; i++) {
+            ReservationRequest request = getSpecificReservationRequest(savedPerformance.getId(),
+                    savedPerformance.getName(), 100000, 1, "A", i);
+
+            reservationService.reserve(request);// 물론 여기서도 오류가 나오면 안됨
+        }
+
+        List<Reservation> reservations = reservationRepository.findByPerformanceId(savedPerformance.getId());
+        Reservation reservation1 = reservations.get(0);
+        Reservation reservation2 = reservations.get(1);
+
+        System.out.println("==============공연 취소 전====================");
+        System.out.println("reservation1 : " + reservation1);
+        System.out.println();
+        System.out.println("reservation2 : " + reservation2);
+        System.out.println("==============공연 취소 후====================");
+
+        // when
+        performanceService.performanceCancel(savedPerformance.getId());
+
+        Reservation recallReservation1 = reservationRepository.findById(reservation1.getId()).get();
+        Reservation recallReservation2 = reservationRepository.findById(reservation2.getId()).get();
+        System.out.println("recallReservation1 : " + recallReservation1);
+        System.out.println();
+        System.out.println("recallReservation2 : " + recallReservation2);
+
+        // then
+        Performance performance1 = performanceRepository.findByIdNotNull(performance.getId())
+                .orElseThrow(IllegalArgumentException::new);
+        System.out.println(performance1);
+        Assertions.assertThat(performance1.getDeletedAt()).isNotNull();
+        Assertions.assertThat(recallReservation1.getRefundSeats().length()).isGreaterThan(0);
+        Assertions.assertThat(recallReservation2.getRefundSeats().length()).isGreaterThan(0);
+    }
+
+    /**
+     * 공연 Fixture 생성
+     * @param performanceName 게르테나 미술관
+     * @param price 20000
+     * @param round 1
+     * @param type 0
+     * @param startDate 현재시간
+     * @param isReserve enable
+     * @return Performance
+     */
+    private Performance getSamplePerformance(String performanceName, int price, int round, int type, LocalDateTime startDate, String isReserve) {
+        return Performance.builder()
+                .name(performanceName)
+                .price(price)
+                .round(round) // 1회차
+                .type(type)  // 0: 전시회, 1: 공연
+                .startDate(startDate)
+                .isReserve(isReserve)
                 .build();
+    }
 
-        // 공연좌석 정보를 저장한다.
-        PerformanceSeatInfo seat = new PerformanceSeatInfo(1, performance, 1, 2, "a", 3);
-        performance.reserveSeat(seat);
+    /**
+     * 공연 좌석 Fixture 생성
+     * @param performance 게르테나 미술관
+     * @param seat 좌석 열
+     * @return PerformanceSeatInfo
+     */
+    private PerformanceSeatInfo getSampleSeat(Performance performance, int seat) {
+        return PerformanceSeatInfo.builder()
+                .seat(seat)
+                .performance(performance)
+                .round(1)
+                .isReserve("enable")
+                .gate(1)
+                .line("A")
+                .build();
+    }
 
-        UUID uuid = performanceRepository.save(performance).getId();
-        performanceSeatInfoRepository.save(seat);
+    /**
+     * 예약 Fixture 생성
+     */
+    private ReservationRequest getReservationRequest(UUID performanceId, String performanceName, long balance, boolean more) {
+        ArrayList<PerformanceSeat> seatList = new ArrayList<>();
+        PerformanceSeat performanceSeat = new PerformanceSeat(1, "A", 1, "enable");
+        seatList.add(performanceSeat);
+        if (more) {
+            PerformanceSeat performanceSeat2 = new PerformanceSeat(1, "A", 2, "enable");
+            seatList.add(performanceSeat2);
 
-        Performance performance1 = performanceRepository.findById(uuid).get();
-        System.out.println(performance1.getReserveSeat());
+            PerformanceSeat performanceSeat3 = new PerformanceSeat(1, "A", 3, "enable");
+            seatList.add(performanceSeat3);
+        }
+
+        ReservationRequest request = new ReservationRequest();
+        request.setPerformId(performanceId);
+        request.setPerformName(performanceName);
+        request.setSeatList(seatList);
+        request.setRound(1);
+        request.setBalance(balance);
+        request.setUserName(USER_NAME);
+        request.setPhoneNumber(PHONE_NUMBER);
+
+        return request;
+    }
+
+    /**
+     * 특정 좌석 예약 Fixture (1개의 좌석만 예약하는 경우)
+     */
+    private ReservationRequest getSpecificReservationRequest(UUID performanceId, String performanceName, long balance, int gate, String line, int seat) {
+        ArrayList<PerformanceSeat> seatList = new ArrayList<>();
+        PerformanceSeat performanceSeat = new PerformanceSeat(gate, line, seat, "enable");
+        seatList.add(performanceSeat);
+
+        ReservationRequest request = new ReservationRequest();
+        request.setPerformId(performanceId);
+        request.setPerformName(performanceName);
+        request.setSeatList(seatList);
+        request.setRound(1);
+        request.setBalance(balance);
+        request.setUserName(USER_NAME);
+        request.setPhoneNumber(PHONE_NUMBER);
+
+        return request;
     }
 }
